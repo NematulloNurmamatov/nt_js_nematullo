96e5a5b23efe7db774efb12175bb59cf89d7666a
NematulloNurmamatov <nematullohataboev@gmail.com>
2024-10-12 12:34:08 +0500

    first commit

---
+++ dars1.js
@@ -0,0 +1,132 @@
+// --------------------------------------------------------------- masala-1
+
+// let a = prompt('kvadratning tomonini kiriting');
+// let P;
+
+// P = a * 4;
+// console.log('kvadratning perimetri ' + P);
+
+// --------------------------------------------------------------- masala-2
+
+// let a = prompt('kvadratning yuzasini kiriting');
+// let S;+
+
+// S = a * a;
+// console.log('kvadratning yuzasi ' + S);
+
+// --------------------------------------------------------------- masala-3
+
+// let a = +prompt('a tomonini kiriting');
+// let b = +prompt('b tomonini kiriting');
+
+// let P;
+// let S;
+
+// P = 2 * (a + b);
+// S = a * b;
+// console.log('perimetri ' + P);
+// console.log('yuzasi ' + S);
+
+
+// --------------------------------------------------------------- masala-4
+
+// let d = prompt('l tomonini kiriting');
+// const pi = 3.14;
+// let l;
+
+// l = d * pi;
+
+// console.log('uzunligi ' + l);
+
+
+// --------------------------------------------------------------- masala-5
+
+// let a = +prompt('a tomonini kiriting');
+
+// let V = a * a * a;
+// let S = 6 * a * a;
+
+// console.log('hajmi ' + V);
+// console.log('sirti ' + S);
+
+
+// --------------------------------------------------------------- masala-6
+
+// let a = +prompt('a tomonini kiriting');
+// let b = +prompt('b tomonini kiriting');
+// let c = +prompt('c tomonini kiriting');
+
+// let V = a * b * c;
+// let S = 2 * (a * b + b * c + a * c);
+
+// console.log('hajmi ' + V);
+// console.log('tola sirti ' + S);
+
+
+// --------------------------------------------------------------- masala-7
+
+// let R = +prompt('r ni kiriting');
+// let pi = 3.14
+
+// let L = 2 * pi * R;
+// let S = pi * (R * R);
+
+// console.log('uzunligi ' + L);
+// console.log('yuzasi ' + S);
+
+
+// --------------------------------------------------------------- masala-8
+
+// let a = +prompt('a tomonini kiriting');
+// let b = +prompt('b tomonini kiriting');
+
+// let K = (a + b) / 2;
+
+// console.log('orta arifmetiki ' + K);
+
+
+// --------------------------------------------------------------- masala-9
+
+// let a = +prompt('a tomonini kiriting');
+// let b = +prompt('b tomonini kiriting');
+
+// let i = a * b / 2;
+
+// console.log('orta geometrigi ' + i);
+
+
+// --------------------------------------------------------------- masala-10
+
+// let a = +prompt('a tomonini kiriting');
+// let b = +prompt('b tomonini kiriting');
+
+// let y = a + b;
+// let k = a * b;
+// let kv = a * a;
+// let kvv = b * b;
+// console.log('toplami ' + y);
+// console.log('kopaytmasi ' + k);
+// console.log('a ning kvadrati ' + kv);
+// console.log('b ning kvadrati ' + kvv);
+
+
+// --------------------------------------------------------------- masala-11
+
+// const a = +prompt('a ni kiriting');
+// const b = +prompt('b kiriting');
+
+// const modulus = a % b;
+
+// console.log('moduli: '+ modulus); 
+
+
+// ---------------------------------------------------------------- masala-12
+
+// let a = +prompt('a ni kiriting');
+// let b = +prompt('b ni kiriting');
+
+// let c = (a * a + b * b) / 2;
+// let p = (a + b + c);
+
+// console.log(c);
+// console.log(p);

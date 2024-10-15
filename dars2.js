e679c90c72b30772a61e2ccd39672cc01d8c0c2d
NematulloNurmamatov <nematullohataboev@gmail.com>
2024-10-15 20:19:24 +0500

    first commit

---
+++ dars2.js
@@ -0,0 +1,312 @@
+// ------------------------------------------------------------- lesson 
+
+// let oy = +prompt('oy raqamini yozing');
+
+// if (oy == 1) {
+//     console.log('yanvar');
+
+// } else if (oy == 2) {
+//     console.log('Fevral');
+
+// }else if (oy == 3) {
+//     console.log('Mart');
+
+// }else if (oy == 4) {
+//     console.log('Aprel');
+
+// }else if (oy == 5) {
+//     console.log('May');
+
+// }else if (oy == 6) {
+//     console.log('Iyun');
+
+// }else if (oy == 7) {
+//     console.log('Iyul');
+
+// }else if (oy == 8) {
+//     console.log('Avgust');
+
+// }else if (oy == 9) {
+//     console.log('Sentyabr');
+
+// }else if (oy == 10) {
+//     console.log('Oktyabr');
+
+// }else if (oy == 11) {
+//     console.log('Noyabr');
+
+// }else if (oy == 12) {
+//     console.log('Decabr');
+
+// } else {
+//     console.log('bumday oy yoq');
+
+// }
+
+
+// let a = +prompt('raqam kiriting');
+
+// if (a > 0) {
+//     a++
+//     console.log(a);
+// } else if (a < 0) {
+//     console.log(a - 2);
+// } else if (a == 0) {
+//     console.log("10");
+// }
+
+// --------------------------------------------------------------------------- homework
+
+
+// ---------------------------------------------------- masala1
+
+// let a = +prompt('raqam kiriting');
+
+// if (a > 0) {
+//     a++
+//     console.log(a++);
+// } else {
+//     console.log(a);
+// }
+
+// ---------------------------------------------------- masala2
+
+// let a = +prompt('raqam kiriting');
+
+// if (a > 0) {
+//     a++
+//     console.log(a++);
+// } else {
+//     console.log(a - 2);
+// }
+
+
+// ---------------------------------------------------- masala3
+
+// let a = +prompt('raqam kiriting');
+
+// if (a > 0) {
+//     a++
+//     console.log(a);
+// } else if (a < 0) {
+//     console.log(a - 2);
+// } else if (a == 0) {
+//     console.log("10");
+// }
+
+// ---------------------------------------------------- masala4
+
+// let son1 = +prompt("birinchi son kiriting ");
+// let son2 = +prompt("ikkinchi son kiriting ");
+// let son3 = +prompt("uchinchi son kiriting ");
+
+
+// let musbat_sanoq = 0;
+// let manfiy_sanoq = 0;
+
+// if (son1 > 0) {
+//     musbat_sanoq++;
+// } else if (son1 < 0) {
+//     manfiy_sanoq++;
+// }
+
+// if (son2 > 0) {
+//     musbat_sanoq++;
+// } else if (son2 < 0) {
+//     manfiy_sanoq++;
+// }
+
+// if (s
+//     musbat_sanoq++;
+// } else if (son3 < 0) {
+//     manfiy_sanoq++;
+// }
+
+
+// console.log('musbat son ' + musbat_sanoq + 'ta');
+
+// ---------------------------------------------------- masala5
+
+// let son1 = +prompt("birinchi son kiriting ");
+// let son2 = +prompt("ikkinchi son kiriting ");
+// let son3 = +prompt("ikkinchi son kiriting ");
+
+// let musbat_sanoq = 0;
+// let manfiy_sanoq = 0;
+
+// if (son1 > 0) {
+//     musbat_sanoq++;
+// } else if (son1 < 0) {
+//     manfiy_sanoq++;
+// }
+
+// if (son2 > 0) {
+//     musbat_sanoq++;
+// } else if (son2 < 0) {
+//     manfiy_sanoq++;
+// }
+// if (son3 > 0) {
+//     musbat_sanoq++;
+// } else if (son3 < 0) {
+//     manfiy_sanoq++;
+// }
+
+
+// console.log('musbat son ' + musbat_sanoq + 'ta');
+// console.log('manfiy son ' + manfiy_sanoq + 'ta');
+
+
+
+// ---------------------------------------------------- masala6
+
+// let a = +prompt('raqam kirting');
+// let b = +prompt('raqam kirting');
+
+// if (a > b) {
+//     console.log(a + ' soni ' + b + " dan katta");
+    
+// } else {
+//     console.log(b + ' soni ' + a + " dan katta");
+// }
+
+// ---------------------------------------------------- masala7
+
+// let son1 = +prompt("Birinchi butun sonni kiriting:");
+// let son2 = +prompt("Ikkinchi butun sonni kiriting:");
+
+// let kichikSon;
+// let tartibRaqami;
+
+// if (son1 < son2) {
+//     kichikSon = son1;
+//     tartibRaqami = 1; 
+// } else {
+//     kichikSon = son2;
+//     tartibRaqami = 2; 
+// }
+
+// console.log("Kichik son:", kichikSon);
+// console.log("Tartib raqami:", tartibRaqami);
+
+
+// ---------------------------------------------------- masala8
+
+// let a = +prompt('raqam kiriting');
+// let b = +prompt(' ikkinchi raqamni kiriting');
+
+// if (a > b) {
+//     console.log(a, b);
+    
+// } else {
+//     console.log(b, a);
+// }
+
+// ---------------------------------------------------- masala9
+
+// let a = +prompt('1-raqamni kiriting');
+// let b = +prompt('2-raqamni kiriting');
+
+// if (a >= b) {
+//     let temp = a;
+//     a = b;
+//     b = temp;
+// }
+
+// console.log("a: " + a);
+// console.log("b: " + b);
+
+// ---------------------------------------------------- masala10
+
+// let a = +prompt('1-raqamni kiriting');
+// let b = +prompt('2-raqamni kiriting');
+
+// if (a != b) {
+//     console.log(a + b);
+    
+// } else {
+//     console.log(0);
+// }
+
+// console.log("a: " + a);
+// console.log("b: " + b);
+
+// ---------------------------------------------------- masala11
+
+
+// let a = +prompt('1-raqamni kiriting');
+// let b = +prompt('2-raqamni kiriting');
+
+// if (a !== b) {
+//     a = Math.max(a, b);
+//     b = a;
+// } else {
+//     a = 0;
+//     b = 0;
+// }
+
+// console.log("a:", a);
+// console.log("b:", b);
+
+// ---------------------------------------------------- masala12
+
+// let a = +prompt('1-raqamni kiriting');
+// let b = +prompt('2-raqamni kiriting');
+// let c = +prompt('3-raqamni kiriting');
+
+// let min = Math.min(a, b, c);
+
+// console.log("Eng kichik son:", min);
+
+
+// ---------------------------------------------------- masala13
+
+// let son1 = +prompt('1-raqamni kiriting');
+// let son2 = +prompt('2-raqamni kiriting');
+// let son3 = +prompt('3-raqamni kiriting');
+
+// let sonlar = [son1, son2, son3];
+
+// let kichikSon = Math.min(son1, son2, son3);
+// let kattaSon = Math.max(son1, son2, son3);
+
+// let ortacha = (son1 + son2 + son3 - kichikSon - kattaSon) / 1; // Faqat o'rtadagi son
+
+// console.log("Kichik son:", kichikSon);
+// console.log("Katta son:", kattaSon);
+// console.log("O'rtadagi son:", ortacha);
+
+
+// ---------------------------------------------------- masala14
+
+// let son1 = +prompt('1-raqamni kiriting');
+// let son2 = +prompt('2-raqamni kiriting');
+// let son3 = +prompt('3-raqamni kiriting');
+
+// let kichikSon = Math.min(son1, son2, son3);
+// let kattaSon = Math.max(son1, son2, son3);
+
+// console.log("Eng kichik son:", kichikSon);
+// console.log("Eng katta son:", kattaSon);
+
+
+// ---------------------------------------------------- masala15
+
+// let son1 = +prompt('1-raqamni kiriting');
+// let son2 = +prompt('2-raqamni kiriting');
+// let son3 = +prompt('3-raqamni kiriting');
+
+// let sonlar = [son1, son2, son3];
+
+// let yigindilar = [
+//     {sonlar: [son1, son2], yigindi: son1 + son2},
+//     {sonlar: [son1, son3], yigindi: son1 + son3},
+//     {sonlar: [son2, son3], yigindi: son2 + son3}
+// ];
+
+// let engKatta = yigindilar.reduce((max, current) => {
+//     return (current.yigindi > max.yigindi) ? current : max;
+// });
+
+// console.log("Eng katta yig'indi bo'lgan sonlar:", engKatta.sonlar);
+// console.log("Yig'indisi:", engKatta.yigindi);
+

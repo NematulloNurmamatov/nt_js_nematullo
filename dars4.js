aaafbef3500a845dd2b96f9edee6dd5866765cd3
NematulloNurmamatov <nematullohataboev@gmail.com>
2024-10-21 18:15:17 +0500

    first commit

--- dars4.js
+++ dars4.js
@@ -6,7 +6,7 @@ let alisher = +prompt("Alisher, sizda qancha pul bor (so'mda kiriting)?");
 const dollar = 11000.34;
 const euro = 12354.03;
 
-let samolyot = dollar * 500;
+let samalyot = dollar * 500;
 let mehmonxona = dollar * 250;
 let sayr = euro * 120;
 
